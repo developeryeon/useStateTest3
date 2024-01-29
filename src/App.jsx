6e775f5ed50a9d0910016dc9b4900fd6bc536b39
yeon <test2@test2.com>
2024-01-29 13:10:38 +0900

    ex4

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,33 @@
-import React, { useState } from "react";
-import FilterButtons from "./components/FilterButtons";
-import StudentList from "./components/StudentList";
+import React, { useState } from 'react';
+import FilterButtons from './components/FilterButtons';
+import StudentList from './components/StudentList';
 
 function App() {
-  const initialStudents = [
-    { name: "Alice", age: 17, grade: "A" },
-    { name: "Bob", age: 18, grade: "B" },
-    { name: "Charlie", age: 16, grade: "C" },
-    { name: "Diana", age: 19, grade: "D" },
-  ];
+	const initialStudents = [
+		{ name: 'Alice', age: 17, grade: 'A' },
+		{ name: 'Bob', age: 18, grade: 'B' },
+		{ name: 'Charlie', age: 16, grade: 'C' },
+		{ name: 'Diana', age: 19, grade: 'D' },
+	];
 
-  const [filteredStudents, setFilteredStudents] = useState(initialStudents);
+	const [filteredStudents, setFilteredStudents] = useState(initialStudents);
 
-  return (
-    <div>
-      <h1>학생 목록</h1>
-      {/* TODO: FilterButtons 컴포넌트를 작성하고 필요한 props를 전달하세요. */}
-      <FilterButtons
-      /* 필요한 props를 여기에 전달하세요. */
-      />
+	const filterHandler = setFilteredStudents.filter((student) => student.name);
 
-      {/* TODO: StudentList 컴포넌트를 작성하고 필요한 props를 전달하세요. */}
-      <StudentList
-      /* 필요한 props를 여기에 전달하세요. */
-      />
-    </div>
-  );
+	return (
+		<div>
+			<h1>학생 목록</h1>
+			{/* TODO: FilterButtons 컴포넌트를 작성하고 필요한 props를 전달하세요. */}
+			<FilterButtons filterHandler={filterHandler} />
+
+			{/* TODO: StudentList 컴포넌트를 작성하고 필요한 props를 전달하세요. */}
+			{
+				/* <StudentList
+			/* 필요한 props를 여기에 전달하세요. */
+				/> */
+			}
+		</div>
+	);
 }
 
 export default App;
