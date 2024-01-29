862e1f325adbc52824179305aaead5841e32305e
yeon <test2@test2.com>
2024-01-29 12:41:56 +0900

    ex1

--- src/App.jsx
+++ src/App.jsx
@@ -16,11 +16,13 @@ function App() {
 			<h1>학생 목록</h1>
 			<ul>
 				{/* TODO: map을 사용해서 filteredStudents를 여기에 렌더링하세요. */}
-				filteredStudents.map((student, index) => (
-				<li value={index}>
-					<button onClick={(student) => alert(`age: ${student.age} grade: ${student.grade}`)} />
-				</li>
-				)){/* TODO: 학생이름을 클릭하면 나이와 점수가 alert 돼야 해요.*/}
+				{filteredStudents.map((student, index) => (
+					<li key={index}>
+						<button onClick={() => alert(`age: ${student.age} grade: ${student.grade}`)}>학생</button>
+					</li>
+				))}
+
+				{/* TODO: 학생이름을 클릭하면 나이와 점수가 alert g돼야 해요.*/}
 			</ul>
 		</div>
 	);
